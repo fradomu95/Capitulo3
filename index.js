2d2bc52fc2c0dbb6eec0565ae34c32b956cf171f
Francisco <francisco.dominguez.munoz@iesvegademijas.es>
2020-11-19 10:25:36 +0100

    primera subida

---
+++ index.js
@@ -0,0 +1,22 @@
+let hatPrice =100;
+console.log(`Hat price: ${hatPrice}`);
+let bootsPrice = "100";
+console.log(`Boots price: ${bootsPrice}`);
+
+if (hatPrice === bootsPrice) {
+    console.log("Prices are the same");
+} else {
+    console.log("Prices are different");
+}
+
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
+console.log(`Total Price: ${totalPrice}`);
+
+let myVariable = "Adam";
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
+
+let firstCity;
+let secondCity = firstCity || "London";
+console.log(`City: ${secondCity}`);
